a5a6927ea1000c687e4b32c02e04ec2f4c0a3396
Nikita Kirsanov <nikita_kirsanov@epam.com>
2015-05-18 10:36:45 +0300

    reviews view was updated

--- app/components/crucible/reviews.controller.js
+++ app/components/crucible/reviews.controller.js
@@ -5,22 +5,33 @@
         .module('forcePluginApp')
         .controller('ReviewsController', ReviewsController);
 
-    ReviewsController.$inject = ['$scope', '$q', 'crucible', 'whoIsService', 'REVIEW_TYPE'];
+    ReviewsController.$inject = ['$scope', '$q', 'crucible', 'REVIEW_TYPE'];
 
-    function ReviewsController($scope, $q, crucible, whoIs, REVIEW_TYPE) {
+    function ReviewsController($scope, $q, crucible, REVIEW_TYPE) {
         $scope.loading = true;
+        $scope.reviewsMap = {};
 
-        var toReviewPromise = crucible.getReviews(REVIEW_TYPE.TO_REVIEW)
-            .then(function (reviews) {
-                $scope.reviewsToReview = reviews;
-            });
+        $scope.reviewsNames = {};
+        $scope.reviewsNames[REVIEW_TYPE.READY_TO_CLOSE] = 'Ready to close';
+        $scope.reviewsNames[REVIEW_TYPE.OUT_FOR_REVIEW] = 'Out for review';
+        $scope.reviewsNames[REVIEW_TYPE.TO_REVIEW] = 'To review';
+        $scope.reviewsNames[REVIEW_TYPE.DRAFTS] = 'Drafts';
 
-        var outForReviewPromise = crucible.getReviews(REVIEW_TYPE.OUT_FOR_REVIEW)
-            .then(function (reviews) {
-                $scope.outForReview = reviews;
-            });
+        var reviewTypesToLoad = [REVIEW_TYPE.READY_TO_CLOSE,
+            REVIEW_TYPE.OUT_FOR_REVIEW,
+            REVIEW_TYPE.TO_REVIEW,
+            REVIEW_TYPE.DRAFTS];
 
-        $q.all([toReviewPromise, outForReviewPromise])
+        reviewTypesToLoad
+            .reduce(function (promises, reviewType) {
+                return promises.then(function () {
+                    return crucible.getReviews(reviewType)
+                        .then(function (reviews) {
+                            $scope.reviewsMap[reviewType] = reviews;
+                        });
+                })
+
+            }, $q.when(1))
             .then(function () {
                 $scope.loading = false;
             });
